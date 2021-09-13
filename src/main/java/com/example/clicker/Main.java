412ded92c05ad2cdec21715f148cecc4d4f6d5a9
CotRa2032409 <2032409@etu.cegepjonquiere.ca>
2021-09-13 15:39:01 -0400

    Fini

--- src/main/java/com/example/clicker/Main.java
+++ src/main/java/com/example/clicker/Main.java
@@ -1,12 +1,16 @@
 package com.example.clicker;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -14,47 +18,150 @@ public class Main extends Application {
 
     private int compteur = 0;
     private int upgradePtsd = 0; //C'est tout à fait normal qu'il y ait un "D" en trop
-    private int essai = 125;
+    private int prixPlusClics = 125;
+    private int prixAutoClics = 2000;
+    private int ptsAutoClics = 2;
+    private boolean remise = false;
+
+
     @Override
+
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Clicker!");
+        final var temps = new PauseTransition(Duration.seconds(1));
+        var timer = new SequentialTransition(temps);
+
+        stage.setTitle("Greedier Mode");
 
         //Bouton Clicker
-        Button bouton = new Button("Clique içi!");
+        Button bouton = new Button("Wooden Nickel");
         bouton.setPrefSize(150, 150);
         bouton.setTranslateY(50);
 
+
         //Création des Labels
-        Label nbPoints = new Label("Points : ");
+        Label nbPoints = new Label("Penny : ");
         Label points = new Label("0");
-        Label upTexte1 = new Label(125 + " pts");
+        Label upTexte1 = new Label(125 + " $$$");
+        Label upTexte2 = new Label(500 + " $$$");
+        Label upTexte3 = new Label(750 + " $$$");
+        Label upTexte4 = new Label(1000 + " $$$");
+        Label upTexte5 = new Label(2000 + " $$$");
+
+        //Debug button
+        /*Button debug = new Button("Blank Card + Car Battery + Tarot Cloth + 2 of Diamonds");
+        debug.setTranslateY(250);
+        debug.setOnAction((actionEvent) -> {
+            points.setText(Integer.toString(compteur += 10000000));
+        });*/
 
         //Modif des Labels
         upTexte1.setTranslateX(360);
         upTexte1.setTranslateY(25);
+        upTexte2.setTranslateX(360);
+        upTexte2.setTranslateY(55);
+        upTexte3.setTranslateX(360);
+        upTexte3.setTranslateY(85);
+        upTexte4.setTranslateX(360);
+        upTexte4.setTranslateY(115);
+        upTexte5.setTranslateX(360);
+        upTexte5.setTranslateY(145);
         points.setTranslateX(50);
 
         //Création des upgrades et modifications
-        Button upgrade1 = new Button("Plus de points en cliquant");
-        upgrade1.setTranslateY(20);
-        upgrade1.setTranslateX(200);
+        Button plusClic = new Button("1 + 1 Free 4 Evar");
+        Button bigGamble = new Button("Slot Machine");
+        Button ptsRendus = new Button("Steam Sales");
+        Button unlock = new Button("Mom's Key");
+        Button auto = new Button("Broken Watch");
+        plusClic.setPrefSize(152,20);
+        plusClic.setTranslateY(20);
+        plusClic.setTranslateX(200);
+        bigGamble.setPrefSize(152, 20);
+        bigGamble.setTranslateY(50);
+        bigGamble.setTranslateX(200);
+        ptsRendus.setPrefSize(152, 20);
+        ptsRendus.setTranslateY(80);
+        ptsRendus.setTranslateX(200);
+        unlock.setPrefSize(152, 20);
+        unlock.setTranslateY(110);
+        unlock.setTranslateX(200);
+        auto.setPrefSize(152, 20);
+        auto.setTranslateY(140);
+        auto.setTranslateX(200);
+        auto.setDisable(true);
+
 
         //setOnAction
         bouton.setOnAction((event) -> {
             compteur();
             points.setText(Integer.toString(compteur));
         });
-        upgrade1.setOnAction((actionEvent) -> {
-            if (compteur >= essai) {
-                upgradePtsd++;
-                points.setText(Integer.toString(compteur -= essai));
-                essai *= 2;
-                upTexte1.setText(essai + " pts");
+        plusClic.setOnAction((actionEvent) -> {
+            if (compteur >= prixPlusClics) {
+                upgradePtsd += 1;
+                if (remise == true) {
+                    compteur -= prixPlusClics * 0.9;
+                } else
+                    compteur -= prixPlusClics;
+                points.setText(Integer.toString(compteur));
+                prixPlusClics *= 2;
+                upTexte1.setText(prixPlusClics + " pts");
+            }
+        });
+        bigGamble.setOnAction((actionEvent) -> {
+            if (compteur >= 500) {
+                if (remise == true) {
+                    compteur -= 500 * 0.9;
+                } else
+                    compteur -= 500;
+                if (Math.random() * 1 >= 0.5) {
+                    compteur = 0;
+                }
+                points.setText(Integer.toString(compteur));
+            }
+        });
+        ptsRendus.setOnAction((actionEvent) -> {
+            if (compteur >= 750) {
+                points.setText(Integer.toString(compteur -= 750));
+                remise = true;
+                ptsRendus.setDisable(true);
+            }
+        });
+        unlock.setOnAction((actionEvent) -> {
+            if (compteur >= 1000) {
+                unlock.setDisable(true);
+                auto.setDisable(false);
+                if (remise == true) {
+                    compteur -= 1000 * 0.9;
+                } else
+                    compteur -= 1000;
+                points.setText(Integer.toString(compteur));
+            }
+        });
+        auto.setOnAction((actionEvent) -> {
+            if (compteur >= prixAutoClics) {
+                if (remise == true)
+                    compteur -= prixAutoClics * 0.9;
+                else
+                    compteur -= prixAutoClics;
+                points.setText(Integer.toString(compteur));
+                ptsAutoClics *= 2;
+
+                temps.setOnFinished(event -> {
+                    compteur = compteur + ptsAutoClics;
+                    points.setText(String.valueOf(compteur));
+                });
+                timer.setCycleCount(PauseTransition.INDEFINITE);
+                timer.play();
+
+                prixAutoClics *= 2;
+                upTexte5.setText(prixAutoClics + " pts");
             }
         });
 
         //Scene
-        Group root = new Group(bouton, nbPoints, points, upgrade1, upTexte1);
+        Group upgrades = new Group(plusClic, upTexte1, bigGamble, upTexte2, ptsRendus, upTexte3, unlock, upTexte4, auto, upTexte5);
+        Group root = new Group(bouton, nbPoints, points, upgrades/*, debug*/);
         stage.setScene(new Scene(root));
         stage.show();
     }
@@ -64,7 +171,7 @@ public class Main extends Application {
     }
 
     public void compteur() {
-        compteur += 1 + upgradePtsd;
+        compteur += 1 + (upgradePtsd * 5);
     }
 }
 
